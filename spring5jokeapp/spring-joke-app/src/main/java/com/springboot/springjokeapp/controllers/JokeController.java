44ce1bfbdc1bdff19653c05b17b4a296894d3aef
vidhita <vidhita@outlook.com>
2020-11-23 15:34:15 +0530

    Changes to display joke using Chuck Norris library

---
+++ spring5jokeapp/spring-joke-app/src/main/java/com/springboot/springjokeapp/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package com.springboot.springjokeapp.controllers;
+
+import com.springboot.springjokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class JokeController {
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
---
+++ spring5jokeapp/spring-joke-app/src/main/java/com/springboot/springjokeapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.springboot.springjokeapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
